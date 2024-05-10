565b3bb3cd161e5bc3f657268f23db19fbdf74bb
Kinsey Philips <kphilips@causltd.com>
2024-05-10 16:37:13 -0300

    trying to get other category to work

--- inspections_graphing/graphing/views.py
+++ inspections_graphing/graphing/views.py
@@ -4,10 +4,29 @@ from django.shortcuts import render
 from graphing.models import Graph
 
 def chart(request):
+    Graph.objects.get_or_create(name='Other', count=0)
     df = Graph.objects.all()
-    
-    fig = px.pie(df, values=[d.count for d in df], names=[d.name for d in df])
+    o = Graph.objects.get(name='Other')
+    #other = {}
+        
+    total = 0
+    for x in df:
+        total += x.count
+    min = total * 0.05
 
+    tmp = 0
+    for d in df:
+        if d.count < min:
+            # other.update({d.name : d.count})
+            o.count += d.count
+            # o = Graph.objects.get(name='Other')
+            # tmp = o.count + d.count
+            # o.count = tmp
+            o.save()
+            
+           
+
+    fig = px.pie(df, values=[d.count for d in df], names=[d.name for d in df])
     chart = fig.to_html()
     context = {'chart': chart}
 
