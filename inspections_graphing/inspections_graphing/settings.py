91bd061d518d1dec3ced31bed6b249ea42c5f67d
Kinsey Philips <kphilips@causltd.com>
2024-05-13 09:22:21 -0300

    minor styling

--- inspections_graphing/inspections_graphing/settings.py
+++ inspections_graphing/inspections_graphing/settings.py
@@ -107,6 +107,8 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+STATIC_ROOT = BASE_DIR / 'static_collected'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
